bb8ff32d499c0054219182b17a8ec34bdb77f742
amit dubey <amitdubey6261@gmail.com>
2023-04-04 22:08:37 +0530

    init

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        name : "building" , 
+        type : "glbModel" ,
+        path : "/models/building.glb" ,
+    } ,
+    
+    {
+        name : "obstacle" , 
+        type : "glbModel" ,
+        path : "/models/obstacle.glb" ,
+    } ,
+
+    {
+        name : "ground" , 
+        type : "glbModel" ,
+        path : "/models/ground.glb" ,
+    } ,
+
+    {
+        name : "character" , 
+        type : "glbModel" ,
+        path : "/models/character.glb" ,
+    } ,
+]
