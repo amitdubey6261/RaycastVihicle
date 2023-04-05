cdde410cb7197a9d91b9e09a2284930c16b58091
Amit Dubey <82256362+amitdubey6261@users.noreply.github.com>
2023-04-05 15:05:26 +0530

    Update World.js

--- Experience/World/World.js
+++ Experience/World/World.js
@@ -54,7 +54,8 @@ export default class World {
 
     createBall() {
         this.carBodyCylinder = new CANNON.Cylinder(1 , 1 ,  0.4 , 50 );
-        this.carBodyCylinder.quaternion.setFromEuler(Math.PI/2 , 0 , 0);
+        this.carBodyCylinder.quaternion.setFromEuler(Math.PI / 2 , 0 , 0);
+        //this.ground.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
         this.wheel1 = new CANNON.Body({
             mass: 1,
             //shape: new CANNON.Sphere(1),
